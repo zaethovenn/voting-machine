ecb8d03de1f94edb54f89e0ec941316d7455cf45
Robert McMaster <>
2021-01-30 13:59:23 -0500

    added main method to main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,1 +1,18 @@
 package src.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ----- MAIN MENU ----- \n");
+        System.out.println("1. Cast Votes");
+        System.out.println("2. View Votes on Blockchain");
+        System.out.println("3. Count Votes");
+        System.out.println("0. Exit\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your choice: ");
+        int ch = scanner.nextInt();
+
+    }
