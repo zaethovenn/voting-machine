768429185ab4527ecfff6f3db67c519bd271069e
kailaniwoodard <woodardk@allegheny.edu>
2021-02-02 13:20:17 -0500

    Commently code, beautifully if I may say

--- src/Main.java
+++ src/Main.java
@@ -10,62 +10,79 @@ public class Main {
     System.out.println("1. Cast Votes");
     System.out.println("2. View Votes on Blockchain");
 
-    // 4 total options
+    // Create scanner object to read user input from the terminal
     Scanner sc = new Scanner(System.in);
 
-    // print out empty spaace for users input
-    //
-
-    System.out.println("********************");
+    System.out.println("********************"); // Asterisks for style
+    // Prompt user for input
     System.out.println("Please input your choice: ");
-    char action = Character.toUpperCase(sc.next().charAt(0));
+    char action = Character.toUpperCase(sc.next().charAt(0)); // Save input to variable action
 
+    // Create HashSet to ensure there are no repeat votes according to VoterID
     HashSet<String> hashVotes = new HashSet<>();
+    // Create BlockList to store votes as a Blockchain
     ArrayList<Block> blockList = new ArrayList<>();
+    // Create "Genesis Block" to be the first block of the chain
     Block genesisBlock = new Block(0, "", "", "");
-    blockList.add(genesisBlock);
+    blockList.add(genesisBlock); // Add genesisBlock to the blockchain
 
     // Candidates ArrayList for potential candidates to vote for
     ArrayList<String> candidates = new ArrayList<>();
+    // Add candidates to the candidates ArrayList
     candidates.add("Kanye West");
     candidates.add("Sir Robert Burnetts");
     candidates.add("Chiddy Bang");
 
+    // Initialize variables necessary as parameters for the Blockchain
     String voterID = "";
     String voterName = "";
     String voteParty = "";
     int prevHash = 0;
 
-    // Starting structures of accesible menu
+    // Do-while loop to ensure the program runs through at least once
     do {
-      if (action == 0) {
-        break;
-      } else if (action == '1') {
+      // Option 1: Voting for Candidates
+      if (action == '1') {
+        // Greet users into the Voting Machine
         System.out.println("Welcome to the Voting Machine!");
+        // Prompt user for voter ID
         System.out.println("Enter voter ID: ");
-        sc.nextLine();
-        voterID = sc.nextLine();
+        sc.nextLine(); // Lineskip to fix strange error
+        voterID = sc.nextLine(); // Save ID to voterID
+        // Prompt user for their name
         System.out.println("Enter voter name: ");
-        voterName = sc.nextLine();
-        System.out.println("**** Vote for Candidates ****");
+        voterName = sc.nextLine(); // Save name to voterName
 
+        // Indicate break to show list of candidates
+        System.out.println("\n**** Vote for Candidates ****");
+
+        // Print candidates to the terminal for voters to see
         for (int i = 0; i < candidates.size(); i++) {
           System.out.println((i+1) + ". " + candidates.get(i));
         }
 
+        // Prompt user for their vote
         System.out.println("Enter your vote: ");
-        voteParty = sc.nextLine();
+        voteParty = sc.nextLine(); // Save vote to voteParty
+        // Add vote to the blockchain
         Block vote = new Block(prevHash, voterID, voterName, candidates.get(Integer.parseInt(voteParty)-1));
 
+        // Ensure vote is properly valid by means of no repeated voter IDs
         if (checkValidity(vote, hashVotes)) {
-          hashVotes.add(voterID);
-          prevHash = vote.getBlockHash();
-            blockList.add(vote);
+          hashVotes.add(voterID); // Add voterID to hashset of hashVotes
+          prevHash = vote.getBlockHash(); // previousHash = the current block hash
+          // Add vote to blockchain
+          blockList.add(vote);
+        // if voterID already exists within the hashset
         } else {
+          // Print alert to user
           System.out.println("Vote invalid!");
         }
+
+        // Prompt user to continue voting or to stop
         System.out.println("\nDo you want to continue voting? (Y/N)");
-        char answer = Character.toUpperCase(sc.next().charAt(0));
+        char answer = Character.toUpperCase(sc.next().charAt(0)); // Save answer to answer variable
+        // If answer is no, display main menu again
         if (answer == 'N') {
           // Display menu for user to view their options
           System.out.println("\n**** MAIN MENU ****");
@@ -74,19 +91,25 @@ public class Main {
           System.out.println("0. Exit Program");
           System.out.println("********************");
           System.out.println("Please input your choice: ");
-          action = Character.toUpperCase(sc.next().charAt(0));
+          action = Character.toUpperCase(sc.next().charAt(0)); // Save action to action variable
+          // If action = 0, then quit the program
           if (action == '0') {
             break;
           }
         }
+
+      // Option 2: Display Votes on the Blockchain
       } else if (action == '2') {
+        // Header for displaying votes
         System.out.println("\n**** Displaying Votes ****\n");
 
+        // for loop to continuously display votes
         for (int i = 1; i < blockList.size(); i++) {
-            System.out.println(blockList.get(i).toString());
-            System.out.println("\n**************************\n");
+          System.out.println(blockList.get(i).toString());
+          System.out.println("\n**************************\n"); // Asterisks for pretty
         }
 
+        // Alert user that they are being defaulted back to the main menu
         System.out.println("\nDefaulting back to main menu...\n");
 
         System.out.println("\n**** MAIN MENU ****");
@@ -95,23 +118,25 @@ public class Main {
         System.out.println("0. Exit Program");
         System.out.println("********************");
         System.out.println("Please input your choice: ");
-        action = Character.toUpperCase(sc.next().charAt(0));
+        action = Character.toUpperCase(sc.next().charAt(0)); // Save action to action variable
+        // If action = 0, then quit the program
         if (action == '0') {
           break;
         }
       }
-    } while (true);
+    } while (true); // Continously operate this do-while loop until user elects to quit
 
+    // Thank user for using the Voting Machine
     System.out.println("Thank you for using the Voting Machine!");
 
   }
 
+  /** checkValidity method evaluates if the voterID already exists within the HashSet.
+  */
   public static boolean checkValidity(Block b, HashSet hv) {
     if (hv.contains((String)b.getVote().getVoterID()))
     return false;
     else
     return true;
   }
-
-
 }
