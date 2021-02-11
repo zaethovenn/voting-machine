30e93156d6bde160c0e95160fe89b14163ead473
kailaniwoodard <woodardk@allegheny.edu>
2021-02-10 20:49:13 -0500

    Adding test cases

---
+++ src/test/TestMain.java
@@ -0,0 +1,17 @@
+package src;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import project-group_Kailani_Robert_Zack.Main;
+import org.junit.Test;
+
+public class TestMain {
+
+  @Test
+  public void testValidInputExampleOne() {
+    Main input1 = new Main();
+    char ValidTest = new char('1');
+    boolean isValid = input1.check
+  }
+}
